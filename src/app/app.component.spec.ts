b600639936b1b909b75580ad401d6675b081862c
danielhawkesford <daniel.hawkesford@gmail.com>
2022-12-01 19:34:17 +0000

    Checking Husky is working

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,3 @@
+// Placeholder file to bypass following error:
+// "No inputs were found in config file 'C:/Users/danie/Projects/angular-pokedex/tsconfig.spec.json'. Specified 'include' paths were '["src/**/*.spec.ts","src/**/*.d.ts"]' and 'exclude' paths were '["./out-tsc/spec"]'."
+export {};
