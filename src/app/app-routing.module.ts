30d8ed97cd6c8a1bddacc5698581ae7dfdb80070
danielhawkesford <daniel.hawkesford@gmail.com>
2022-12-03 04:23:04 +0000

    Added routes for detailed pokemon pages and 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+import { DetailedPokemonComponent } from './components/detailed-pokemon/detailed-pokemon.component';
 
 const routes: Routes = [
   { path: '', component: PokemonListComponent },
+  { path: 'pokemon/:id', component: DetailedPokemonComponent },
+  { path: '**', component: PokemonListComponent }
 ];
 
 @NgModule({
